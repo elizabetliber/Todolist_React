b1b194dc6182519fdc33b4768b6fffef63577cd1
Lisa <elizabet.liber@gmail.com>
2020-10-19 01:12:00 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NewTodo from "./NewComponent/NewTodo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tasks: [
+        {id: 1, title: "task №1", done: true},
+        {id: 2, title: "task №2", done: false},
+        {id: 3, title: "task №3", done: false}
+      ],
+      value: '',
+      valueOld: '',
+    }
+    this.onDelete = this.onDelete.bind(this)
+    this.onAdd = this.onAdd.bind(this)
+    this.itemId = 4
+  }
+  
+  onDelete(id) {
+    this.setState(({tasks}) => {
+      return {
+        tasks: tasks.filter((obj) => obj.id !== id)
+      }
+    })
+  }
+  input1Change = (e) => {
+    let next = this.state;
+    next.value = e.target.value;
+    next.changed = next.value !== next.valueOld;
+    this.setState(next)
+    
+  }
+  onAdd(value){
+    const newItem = {
+      id: this.itemId++, title: value, done: false
+    }
+    this.setState(({tasks}) => {
+      const newArr = [...tasks, newItem]
+      return {
+        tasks: newArr
+      }
+    })
+  }
+  
+  render() {
+    const {tasks} = this.state
+    return (
+      <div className="container">
+        <em>Todo List</em>
+        <div className="input-group">
+          <input type="text" data-class="hello"
+                 className="form-control  mr-1"
+                 aria-label="Text input with checkbox"
+                 onChange={this.input1Change}
+                 value={this.state.value}
+          />
+          <button className="btn btn-success"
+                  onClick={() => this.onAdd(this.state.value)}>+ add</button>
+        </div>
+        <div>
+          {tasks.map(task => (
+            <NewTodo task={task}
+                     key={task.id}
+                     onDelete={() => this.onDelete(task.id)}
+                     done={tasks.done}/>
+          ))}
+        </div>
+      </div>
+    )
+  }
 }
+
 
 export default App;
