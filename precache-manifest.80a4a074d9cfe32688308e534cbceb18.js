8902c0f90c029f2fc8e8e0084bf6c3caf4467627
Lisa <elizabet.liber@gmail.com>
2020-10-19 01:17:27 +0700

    Updates

---
+++ precache-manifest.80a4a074d9cfe32688308e534cbceb18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e03114efb3de9f445c3726bd0653006",
+    "url": "/Todolist_React/index.html"
+  },
+  {
+    "revision": "67ad891e0ffff4b780d1",
+    "url": "/Todolist_React/static/css/main.51e87077.chunk.css"
+  },
+  {
+    "revision": "619147c549c1d8919ab6",
+    "url": "/Todolist_React/static/js/2.37cb61a0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Todolist_React/static/js/2.37cb61a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67ad891e0ffff4b780d1",
+    "url": "/Todolist_React/static/js/main.0ddf9e04.chunk.js"
+  },
+  {
+    "revision": "d9e07f9cb13f7bc3a9fb",
+    "url": "/Todolist_React/static/js/runtime-main.ec87546c.js"
+  }
+]);
